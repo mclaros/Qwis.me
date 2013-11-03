d145ae369f2959373dc4a7e94415256368a97800
Mainor <m_claros@live.com>
2013-11-03 14:31:13 -0800

    initialize Qwis.me

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qwisme::Application.config.secret_token = '84874b3e815a1a241813d60e9e21c26d29104ca25adaae4056596c3d9baaad9d908dce767130f34a819abe51587fb640ce36668dadb218cd471e0769acb37040'
