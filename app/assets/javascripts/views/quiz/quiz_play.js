af8397e80cde6c689b820c1f09364e3044f6ed23
Mainor <m_claros@live.com>
2013-11-06 17:24:36 -0800

    finish moving play EJS JS code to play view; untested

--- app/assets/javascripts/views/quiz/quiz_play.js
+++ app/assets/javascripts/views/quiz/quiz_play.js
@@ -2,31 +2,54 @@ Qwisme.Views.QuizPlay = Backbone.View.extend({
 	template: JST["quiz/quiz_play"],
 
 	render: function () {
-		console.log("rendering play");
-		console.log("model is");
-		console.log(this.model);
-
+		var that = this;
 		var renderedTemp = this.template({
 			quiz: this.model,
 			prompts: this.model.get("quiz_prompts")
 		});
 
-		var posAnswers = this.model.posAnswers();
-		var gameData = this.model.get("game_data");
-		var quesToAns = gameData.ques_to_ans;
-		var ansToQues = gameData.ans_to_ques;
-		var $ansContainer = renderedTemp.find("#answers-container");
+		genAnswerDivs(renderedTemp);
 
-		genAnswerDivs($ansContainer);
+		renderedTemp.find("#start-game").on("click", function {
+			event.preventDefault();
+			var $button = $(event.target);
 
+			that.launchQuiz(renderedTemp);
+			renderedTemp.find("#player-input").attr("disabled", false);
+			$button.attr("disabled", true);
+			$button.off();
+		})
 
 		this.$el.html(renderedTemp);
 		return this;
 	},
 
-	listenToInput: function ($inputField) {
-		$inputField.on("keyup keydown", function (event) {
+	launchQuiz: function (renderedView) {
+		this.gameData = this.model.get("game_data");
+		this.quesToAns = gameData.ques_to_ans;
+		this.ansToQues = gameData.ans_to_ques;
+		this.remainingAnsrs = this.model.posAnswers();
 
+		var $ansContainer = renderedView.find("#answers-container");
+
+		listenToInput(renderedView);
+	},
+
+	listenToInput: function (renderedView) {
+		var $inputField = renderedView.find("#player-input");
+
+		$inputField.on("keyup keydown", function (event) {
+			var inputStr = $inputField.val();
+			inputStr = $.trim(inputStr);
+
+			if (isAnswer(inputStr)) {
+				$inputField.val("");
+				trackCorrectGuess(inputStr);
+
+				val $counterDiv = renderedView.find("#correct-count");
+				var newCount = 1 + parseInt($counterDiv.text());
+				$counterDiv.text(newCount);
+			}
 		})
 	},
 
@@ -34,15 +57,28 @@ Qwisme.Views.QuizPlay = Backbone.View.extend({
 		$el.off();
 	},
 
-	genAnswerDivs: function ($container) {
+	trackCorrectGuess: function (guess) {
+		var guessQuestion = this.ansToQues[guess];
+		var validAnswers = this.quesToAns[guessQuestion];
+		var correctAns = _.first(validAnswers);
+
+		revealAns(correctAns);
+		this.remainingAnsrs = _.difference(this.remainingAnsrs, validAnswers);
+	},
+
+	genAnswerDivs: function ($renderedView) {
+		this.ansDivs = {};
+
+		var that = this;
+		var $container = $renderedView
 		var $answerDiv = $("<div>");
 		var $hidAnsText = $("<div>");
 		$answerDiv.addClass("answer-div");
 		$hidAnsText.hide();
 
-		var questions = _.keys(quesToAns);
+		var questions = _.keys(this.quesToAns);
 		_.each(questions, function (question) {
-			var correctAns = _.first(quesToAns[question]);
+			var correctAns = _.first(that.quesToAns[question]);
 			var $newAnswerDiv = $answerDiv.clone();
 			var $newHidAnsText = $hidAnsText.clone();
 
@@ -50,16 +86,16 @@ Qwisme.Views.QuizPlay = Backbone.View.extend({
 			$newAnswerDiv.append($newHidAnsText);
 			$container.append($newAnswerDiv);
 
-			ansDivs[correctAns] = $newAnswerDiv;
+			that.ansDivs[correctAns] = $newAnswerDiv;
 		});
 	},
 
-	isAnswer: function (posAnswers, trimmedInput) {
-		return _.contains(posAnswers, trimmedInput);
+	isAnswer: function (trimmedInput) {
+		return _.contains(this.remainingAnsrs, trimmedInput);
 	},
 
 	var revealAns: function (correctAns) {
-		var $ansDiv = ansDivs[correctAns];
+		var $ansDiv = this.ansDivs[correctAns];
 		var $ansTextDiv = $($ansDiv.find("div"));
 		//animate reveal later
 		$ansDiv.css("background-color", "yellow");
