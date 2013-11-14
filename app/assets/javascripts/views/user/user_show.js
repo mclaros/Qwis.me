4cd49eaec53d76634ed76795699ec4d15159e731
Mainor <m_claros@live.com>
2013-11-14 00:17:38 -0800

    add tabs to user show page

--- app/assets/javascripts/views/user/user_show.js
+++ app/assets/javascripts/views/user/user_show.js
@@ -2,13 +2,16 @@ Qwisme.Views.UserShow = Backbone.View.extend({
 	template: JST["user/user_show"],
 
 	render: function (user) {
-
-		
 		var renderedTemp = this.template({
 			user: this.model
 		});
 
 		this.$el.html(renderedTemp)
+		this.$el.find("#quiz-tabs a").click(function (e) {
+			e.preventDefault();
+			$(this).tab("show");
+		});
+		
 		return this;
 	}
 });
