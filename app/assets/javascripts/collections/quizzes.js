85949343208a6473d7f16281b9f8ba49cb6451a1
Mainor <m_claros@live.com>
2013-11-19 10:14:38 -0800

    add debug code to quizzes collection

--- app/assets/javascripts/collections/quizzes.js
+++ app/assets/javascripts/collections/quizzes.js
@@ -3,10 +3,12 @@ Qwisme.Collections.Quizzes = Backbone.Collection.extend({
 	model: Qwisme.Models.Quiz,
 	
 	initialize: function (modelData, options) {
-		if (options) this.page = options.page;
+		console.log("quizzes collection init");
+		if (options && options.page) this.page = options.page;
 	},
 
 	parse: function (data) {
+		console.log("quizzes collection parse");
 		if (data[0].page) this.page = parseInt(data[0].page);
 		if (data[0].total_pages) this.total_pages = parseInt(data[0].total_pages);
 		return data;
